96876e32f8c59ade47ccbe51c1e50d6a1aa2abd3
keithgw <kwilliams@redventures.com>
2020-03-26 08:51:37 -0400

    fix data typos, add initial matchup plots

---
+++ R/make_matchup_data.R
@@ -0,0 +1,13 @@
+#' Make Matchup Data
+#' From the raw vote data, put votes side-by-side, and calculate the total votes for the matchup
+#'
+#' @param vote_data tibble, the raw data in the form round,director,opponent,votes
+#'
+#' @return tibble, round,director1,directo2,votes1,votes2,total_votes
+make_matchup_data <- function(vote_data) {
+  `%>%` <- magrittr::`%>%`
+
+  dplyr::inner_join(vote_data, vote_data, by = c("round" = "round", "director" = "opponent")) %>%
+    dplyr::select(round, director1 = director, director2 = opponent, votes1 = votes.x, votes2 = votes.y) %>%
+    dplyr::mutate(total_votes = votes1 + votes2)
+}
