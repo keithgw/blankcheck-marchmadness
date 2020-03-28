81c70520de9b5ffddf41bfec61082065e5f8336e
keithgw <kwilliams@redventures.com>
2020-03-28 14:17:19 -0400

    add patreon data

--- R/make_matchup_data.R
+++ R/make_matchup_data.R
@@ -4,10 +4,19 @@
 #' @param vote_data tibble, the raw data in the form round,director,opponent,votes
 #'
 #' @return tibble, round,director1,directo2,votes1,votes2,total_votes
-make_matchup_data <- function(vote_data) {
+make_matchup_data <- function(vote_data, feed=c("main", "patreon")) {
   `%>%` <- magrittr::`%>%`
 
-  dplyr::inner_join(vote_data, vote_data, by = c("round" = "round", "director" = "opponent")) %>%
-    dplyr::select(round, director1 = director, director2 = opponent, votes1 = votes.x, votes2 = votes.y) %>%
+  feed <- match.arg(feed)
+
+  if (feed == "main") {
+    side_by_side <- dplyr::inner_join(vote_data, vote_data, by = c("round" = "round", "director" = "opponent")) %>%
+      dplyr::select(round, director1 = director, director2 = opponent, votes1 = votes.x, votes2 = votes.y)
+  } else {
+    side_by_side <- dplyr::inner_join(vote_data, vote_data, by = c("round" = "round", "franchise" = "opponent")) %>%
+      dplyr::select(round, franchise1 = franchise, franchise2 = opponent, votes1 = votes.x, votes2 = votes.y)
+  }
+
+  side_by_side %>%
     dplyr::mutate(total_votes = votes1 + votes2)
 }
